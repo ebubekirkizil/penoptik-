120a6466c821d662d331b608b05cefbdd1d83074
Ebubekir <ebubekir@sentientwire.com>
2026-08-18 02:56:12 +0300

    Fix Tailwind styling by restoring v3 configuration

--- apps/mini-optik/src/app/admin/inventory/page.tsx
+++
@@ -1,9 +0,0 @@
-import InventoryClient from "./InventoryClient";
-
-export const metadata = {
-  title: "Stok Takibi - Penoptik",
-};
-
-export default function InventoryPage() {
-  return <InventoryClient initialTab="INVENTORY" />;
-}
--- apps/mini-optik/test-db.js
+++
@@ -1,5 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.firm.findFirst({ where: { name: { contains: 'Pen Optik', mode: 'insensitive' } } })
-  .then(f => console.log(f))
-  .finally(() => prisma.$disconnect());
